2dc1c1c8e85808a9e353fe1f22071e4e9df01f58
Sahil Mehta <samehta91@gmail.com>
2018-05-18 01:00:31 -0700

    Add styling to chat box and messages

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -2,11 +2,8 @@ class ChatController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    # Create new array of open sessions if previously opened sessions is empty
     session[:conversations] ||= []
-    # Find all users except current user
     @users = User.all.where.not(id: current_user)
-    # Retrieve all conversations by current user and return only conversations that are in open sessions array
     @conversations = Conversation.includes(:recipient, :messages).find(session[:conversations])
   end
 
